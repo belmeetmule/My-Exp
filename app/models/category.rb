ba365d22ceb5beefd2ca42f9be69073cdeb83a2f
mule <belmeetmule@gmail.com>
2023-02-22 01:34:16 +0300

    add validations and associations to the model classes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :catagoy_expenses, dependent: :destroy
+  has_many :expenses, through: :catagoy_expenses
+
+  validates :name, presence: true
+  validates :icon, presence: true, default: "https://coffee.alexflipnote.dev/random"
 end
