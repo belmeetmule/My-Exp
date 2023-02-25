b0d218c30657fdc07d54f494218d82f102e9232c
mule <belmeetmule@gmail.com>
2023-02-25 18:56:47 +0300

    Add app features

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   #root "home#index"
+  root 'users#splash'
+
+    # devise_scope :user do
+    #   root to: 'devise/sessions#new'
+    # end
+
+    resources :users, only: [:splash]
+  resources :categories, only: [:index, :new, :create, :edit, :update, :destroy], :path => "categories" do 
+    resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy], :path => "transactions"
+  end
 end
